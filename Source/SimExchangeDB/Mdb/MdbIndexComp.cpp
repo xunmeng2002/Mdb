3928a6bb1fc36e61c760bbdbc5566e1ccf8ab0e5
xunmeng2002 <150315803@qq.com>
2025-01-09 15:36:33 +0800

    update

---
+++ Source/SimExchangeDB/Mdb/MdbIndexComp.cpp
@@ -0,0 +1,24 @@
+﻿#include "MdbIndexComp.h"
+#include <string.h>
+
+
+namespace mdb
+{
+	bool PositionEqualForAccountIndex::operator()(const Position* const left, const Position* const right) const
+	{
+		return strcmp(left->TradingDay, right->TradingDay) == 0&& strcmp(left->AccountID, right->AccountID) == 0;
+	}
+	bool PositionLessForAccountIndex::operator()(const Position* const left, const Position* const right) const
+	{
+		if (strcmp(left->TradingDay, right->TradingDay) < 0)
+			return true;
+		else if (strcmp(left->TradingDay, right->TradingDay) > 0)
+			return false;
+		if (strcmp(left->AccountID, right->AccountID) < 0)
+			return true;
+		else if (strcmp(left->AccountID, right->AccountID) > 0)
+			return false;
+		return false;
+	}
+
+}
---
+++ Source/SimExchangeDB/Mdb/MdbIndexComp.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "MdbStructs.h"
+
+namespace mdb
+{
+	struct PositionEqualForAccountIndex
+	{
+		bool operator()(const Position* const left, const Position* const right) const;
+	};
+	struct PositionLessForAccountIndex
+	{
+		bool operator()(const Position* const left, const Position* const right) const;
+	};
+
+}
