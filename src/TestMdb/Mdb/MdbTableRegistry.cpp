a96ec16c1c341e5928a04bb9e9abbb507a6f5bda
xunmeng2002 <150315803@qq.com>
2026-07-24 10:39:42 +0800

    MdbTableRegistry使用代码生成

--- src/TestMdb/Mdb/MdbTableRegistry.cpp
+++ src/TestMdb/Mdb/MdbTableRegistry.cpp
@@ -1,57 +1,52 @@
-#include "MdbTableRegistry.h"
+﻿#include "MdbTableRegistry.h"
 #include "MdbStructs.h"
 
 
 namespace mdb
 {
-
-MdbTableRegistry& MdbTableRegistry::Instance()
-{
-    static MdbTableRegistry instance;
-    return instance;
+	MdbTableRegistry& MdbTableRegistry::Instance()
+	{
+		static MdbTableRegistry instance;
+		return instance;
+	}
+	const TableSchema* MdbTableRegistry::GetSchema(unsigned int tableID) const
+	{
+		switch (tableID)
+		{
+		case TradingDay::TableID:       return &TradingDay::GetSchema();
+		case Exchange::TableID:       return &Exchange::GetSchema();
+		case Product::TableID:       return &Product::GetSchema();
+		case Instrument::TableID:       return &Instrument::GetSchema();
+		case PrimaryAccount::TableID:       return &PrimaryAccount::GetSchema();
+		case Account::TableID:       return &Account::GetSchema();
+		case Capital::TableID:       return &Capital::GetSchema();
+		case Position::TableID:       return &Position::GetSchema();
+		case PositionDetail::TableID:       return &PositionDetail::GetSchema();
+		case Order::TableID:       return &Order::GetSchema();
+		case Trade::TableID:       return &Trade::GetSchema();
+		default:                        return nullptr;
+		}
+	}
+	const TableSchema* const* MdbTableRegistry::GetAllSchemas() const
+	{
+		static const TableSchema* const kAllSchemas[] =
+		{
+			&TradingDay::GetSchema(),
+			&Exchange::GetSchema(),
+			&Product::GetSchema(),
+			&Instrument::GetSchema(),
+			&PrimaryAccount::GetSchema(),
+			&Account::GetSchema(),
+			&Capital::GetSchema(),
+			&Position::GetSchema(),
+			&PositionDetail::GetSchema(),
+			&Order::GetSchema(),
+			&Trade::GetSchema(),
+		};
+		return kAllSchemas;
+	}
+	int MdbTableRegistry::GetTableCount() const
+	{
+		return 11;
+	}
 }
-
-const TableSchema* MdbTableRegistry::GetSchema(unsigned int tableID) const
-{
-    switch (tableID)
-    {
-    case TradingDay::TableID:       return &TradingDay::GetSchema();
-    case Exchange::TableID:         return &Exchange::GetSchema();
-    case Product::TableID:          return &Product::GetSchema();
-    case Instrument::TableID:       return &Instrument::GetSchema();
-    case PrimaryAccount::TableID:   return &PrimaryAccount::GetSchema();
-    case Account::TableID:          return &Account::GetSchema();
-    case Capital::TableID:          return &Capital::GetSchema();
-    case Position::TableID:         return &Position::GetSchema();
-    case PositionDetail::TableID:   return &PositionDetail::GetSchema();
-    case Order::TableID:            return &Order::GetSchema();
-    case Trade::TableID:            return &Trade::GetSchema();
-    default:                        return nullptr;
-    }
-}
-
-const TableSchema* const* MdbTableRegistry::GetAllSchemas() const
-{
-    static const TableSchema* const kAllSchemas[] =
-    {
-        &TradingDay::GetSchema(),
-        &Exchange::GetSchema(),
-        &Product::GetSchema(),
-        &Instrument::GetSchema(),
-        &PrimaryAccount::GetSchema(),
-        &Account::GetSchema(),
-        &Capital::GetSchema(),
-        &Position::GetSchema(),
-        &PositionDetail::GetSchema(),
-        &Order::GetSchema(),
-        &Trade::GetSchema(),
-    };
-    return kAllSchemas;
-}
-
-int MdbTableRegistry::GetTableCount() const
-{
-    return 11;
-}
-
-} // namespace mdb
--- src/TestMdb/Mdb/MdbTableRegistry.h
+++ src/TestMdb/Mdb/MdbTableRegistry.h
@@ -1,23 +1,21 @@
-#pragma once
+﻿#pragma once
 #include <DBAdapters/DBInterface/SchemaRegistry.h>
 
 
 namespace mdb
 {
+	class MdbTableRegistry : public SchemaRegistry
+	{
+	public:
+		static MdbTableRegistry& Instance();
 
-class MdbTableRegistry : public SchemaRegistry
-{
-public:
-    static MdbTableRegistry& Instance();
+		const TableSchema* GetSchema(unsigned int tableID) const override;
+		const TableSchema* const* GetAllSchemas() const override;
+		int GetTableCount() const override;
 
-    const TableSchema* GetSchema(unsigned int tableID) const override;
-    const TableSchema* const* GetAllSchemas() const override;
-    int GetTableCount() const override;
-
-private:
-    MdbTableRegistry() = default;
-    MdbTableRegistry(const MdbTableRegistry&) = delete;
-    MdbTableRegistry& operator=(const MdbTableRegistry&) = delete;
-};
-
-} // namespace mdb
+	private:
+		MdbTableRegistry() = default;
+		MdbTableRegistry(const MdbTableRegistry&) = delete;
+		MdbTableRegistry& operator=(const MdbTableRegistry&) = delete;
+	};
+}
